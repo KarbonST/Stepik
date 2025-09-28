074c429c14db93ac6be3d322c5e808df85baa735
KarbonST <mikhailbykadorov@mail.ru>
2025-09-28 22:14:47 +0300

    delete(main): delete methods from interfaces CarList and CarSet cause parent has this methods

--- src/CarList.java
+++ src/CarList.java
@@ -1,9 +1,5 @@
 public interface CarList extends CarCollection {
     Car get(int index);
-//    void add(Car car);
     boolean add(Car car, int index);
-//    boolean remove(Car car);
     boolean removeAt(int index);
-//    int size();
-//    void clear();
 }
--- src/CarSet.java
+++ src/CarSet.java
@@ -1,6 +1,3 @@
 public interface CarSet extends CarCollection {
-//    boolean add (Car car);
-//    boolean remove(Car car);
-//    int size();
-//    void clear();
+
 }
