04131666c03bd08ce8aedaad555d94be2a0a9902
KarbonST <mikhailbykadorov@mail.ru>
2025-10-22 16:19:06 +0300

    feat(main): work with Tree set

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Set<Integer> numbers = new TreeSet<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return -o1.compareTo(o2);
+            }
+        });
+
+        for (int i = 0; i < 100; i++){
+            numbers.add((int)(Math.random() * 10));
+        }
+
+        for (Integer number: numbers){
+            System.out.println(number);
+        }
     }
 }
