f44ee3ebc034a1c37acfc275ae03665db56747d4
KarbonST <mikhailbykadorov@mail.ru>
2025-10-26 00:44:19 +0300

    test(main): add tests for CarQueue

---
+++ test/CarQueueTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarQueueTest {
+
+    private CarQueue queue;
+
+    @BeforeEach
+    void setUp() {
+        // TODO init
+        for (int i = 0; i < 10; i++){
+            queue.add(new Car(i, "Brand" + i));
+        }
+    }
+
+    @Test
+    void add() {
+        assertEquals(10, queue.size());
+    }
+
+    @Test
+    void peek() {
+        Car expectedCar = new Car(0, "Brand0");
+        assertEquals(expectedCar, queue.peek());
+        assertEquals(10, queue.size());
+    }
+
+    @Test
+    void poll() {
+        Car expectedCar = new Car(0, "Brand0");
+        assertEquals(expectedCar, queue.peek());
+        assertEquals(9, queue.size());
+    }
+}
